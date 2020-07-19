ef9f1658dc1aeeece24d892db103e1da5bcba090
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-19 13:46:26 +0000

    Feat: Additional page for other home tabs styed.

--- screens/Other.jsx
+++ screens/Other.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+import { MainView, colors } from './../components/styles';
+
+const StyledView = styled(MainView)`
+  justify-content: center;
+  align-items: center;
+`;
 
 const Other = () => {
   return (
-    <View>
-      <Text>Other Page</Text>
-    </View>
+    <StyledView>
+      <Text style={{ color: colors.light }}>Extra Page</Text>
+    </StyledView>
   );
 };
 
