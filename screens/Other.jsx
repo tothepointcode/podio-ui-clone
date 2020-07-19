42e1834d1dccfad25be16176cef1f1e6493a3620
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-19 21:55:13 +0000

    Feat: Log out button added to all other pages.

--- screens/Other.jsx
+++ screens/Other.jsx
@@ -1,18 +1,41 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 import { MainView, colors } from './../components/styles';
+const { light, tertiary } = colors;
 
 const StyledView = styled(MainView)`
   justify-content: center;
   align-items: center;
 `;
 
+const StyledButton = styled.TouchableOpacity`
+  background-color: ${tertiary};
+  padding: 12px;
+  border-radius: 30px;
+  margin-vertical: 10px;
+`;
+
+const ButtonText = styled.Text`
+  color: ${light};
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+// Auth context
+import { AuthContext } from './../navigators/AppStack';
+
 const Other = () => {
+  const { _, setAuthenticated } = useContext(AuthContext);
+
   return (
     <StyledView>
-      <Text style={{ color: colors.light }}>Extra Page</Text>
+      <Text style={{ color: light }}>Extra Page</Text>
+      <StyledButton onPress={() => setAuthenticated(false)}>
+        <ButtonText>Log Out</ButtonText>
+      </StyledButton>
     </StyledView>
   );
 };
